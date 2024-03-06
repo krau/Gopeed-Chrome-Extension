037de738fc271f004b1d533fcd8f19bce8811308
krau <krau39@outlook.com>
2024-03-06 12:34:07 +0800

    fix: Ensure Client instance creation after retrieving host and token from Chrome storage

--- src/background.js
+++ src/background.js
@@ -1,33 +1,34 @@
 'use strict';
 import { Client } from "../node_modules/@gopeed/rest";
-let cilent = new Client();
+
 chrome.storage.local.get(['host', 'token'], function (data) {
   const host = data.host;
   const token = data.token;
-  cilent = new Client({
+
+  const client = new Client({
     host: host,
     token: token
   });
-});
 
-chrome.downloads.onCreated.addListener(async function (downloadItem) {
-  console.log('onCreated', downloadItem.finalUrl);
-  chrome.downloads.cancel(downloadItem.id);
-  try {
-    await cilent.createTask({
-      req: {
-        url: downloadItem.finalUrl,
-      },
-      opt: {
-        selectFiles: [0]
-      }
-    });
-  } catch (error) {
-    chrome.notifications.create({
-      type: 'basic',
-      iconUrl: "icons/icon_48.png",
-      title: 'Error when create task',
-      message: 'Error message: ' + error.message,
-    });
-  }
-})
+  chrome.downloads.onCreated.addListener(async function (downloadItem) {
+    console.log('onCreated', downloadItem.finalUrl);
+    chrome.downloads.cancel(downloadItem.id);
+    try {
+      await client.createTask({
+        req: {
+          url: downloadItem.finalUrl,
+        },
+        opt: {
+          selectFiles: [0]
+        }
+      });
+    } catch (error) {
+      chrome.notifications.create({
+        type: 'basic',
+        iconUrl: "icons/icon_48.png",
+        title: 'Error when create task',
+        message: 'Error message: ' + error.message,
+      });
+    }
+  });
+});
