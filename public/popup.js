cfdd69aae46bdd7d64f8107bcb5a74fbea5022fb
krau <krau39@outlook.com>
2024-03-28 11:44:42 +0800

    Refactor

--- public/popup.js
+++ public/popup.js
@@ -2,10 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
     const saveButton = document.getElementById('saveButton');
     const successMessage = document.getElementById('successMessage');
 
+    // 设置默认值
+    chrome.storage.local.get(['enableNotification'], function (result) {
+        if (result.enableNotification === undefined) { // 如果存储中没有enableNotification键
+            chrome.storage.local.set({ enableNotification: true }); // 将默认值设置为true
+            document.getElementById('enableNotification').checked = true; // 设置复选框为选中状态
+        } else {
+            document.getElementById('enableNotification').checked = result.enableNotification; // 否则，根据存储中的值设置复选框状态
+        }
+    });
+
     saveButton.addEventListener('click', function () {
         const host = document.getElementById('host').value;
         const token = document.getElementById('token').value;
-        chrome.storage.local.set({ host: host, token: token }, function () {
+        const enableNotification = document.getElementById('enableNotification').checked;
+        chrome.storage.local.set({ host: host, token: token, enableNotification: enableNotification }, function () {
             successMessage.style.display = 'block';
             setTimeout(function () {
                 successMessage.style.display = 'none';
--- src/background.js
+++ src/background.js
@@ -2,51 +2,67 @@
 
 import { Client } from "../node_modules/@gopeed/rest";
 
-chrome.storage.local.get(['host', 'token'], async function (data) {
-  const host = data.host;
-  const token = data.token;
-  if (!host || !token) {
-    console.error('Missing host or token in storage');
-    return;
-  }
-  const client = new Client({
-    host: host,
-    token: token
+const Settings = {
+  host: 'http://localhost:39666',
+  token: 'qwqowo',
+  enableNotification: true
+}
+
+let client;
+
+const initStorage = chrome.storage.local.get().then((items) => {
+  Object.assign(Settings, items);
+  client = new Client({
+    host: Settings.host,
+    token: Settings.token
   });
+});
 
+chrome.storage.onChanged.addListener((changes) => {
+  if (changes.host) {
+    Settings.host = changes.host.newValue;
+  }
+  if (changes.token) {
+    Settings.token = changes.token.newValue;
+  }
+  if (changes.enableNotification) {
+    Settings.enableNotification = changes.enableNotification.newValue;
+  }
+  client = new Client({
+    host: Settings.host,
+    token: Settings.token
+  });
+});
 
-  chrome.downloads.onDeterminingFilename.addListener(async function (item) {
-    try {
-      await chrome.downloads.cancel(item.id);
-      if (item.state === "complete") {
-        await chrome.downloads.removeFile(item.id);
+chrome.downloads.onDeterminingFilename.addListener(async function (item) {
+  await initStorage;
+  await chrome.downloads.cancel(item.id);
+  try {
+    const downloadUrl = item.finalUrl;
+    await client.createTask({
+      req: {
+        url: downloadUrl,
+      },
+      opt: {
+        name: item.filename,
+        selectFiles: [0]
       }
-      const downloadUrl = item.finalUrl;
-      await client.createTask({
-        req: {
-          url: downloadUrl,
-        },
-        opt: {
-          name: item.filename,
-          selectFiles: [0]
-        }
-      });
+    });
+    if (Settings.enableNotification) {
       await chrome.notifications.create({
         type: 'basic',
         iconUrl: "icons/icon_48.png",
         title: 'Create task success',
         message: 'Size: ' + (item.fileSize / (1024 * 1024)).toFixed(2) + 'MB',
       });
-    } catch (error) {
-      console.error('Error when creating task:', error);
-      if (data.enableNotification) {
-        await chrome.notifications.create({
-          type: 'basic',
-          iconUrl: "icons/icon_48.png",
-          title: 'Error when create task',
-          message: 'Error message: ' + error.message,
-        });
-      }
     }
-  });
+  } catch (error) {
+    await chrome.notifications.create({
+      type: 'basic',
+      iconUrl: "icons/icon_48.png",
+      title: 'Error when create task',
+      message: 'Error message: ' + error.message,
+    });
+  }
 });
+
